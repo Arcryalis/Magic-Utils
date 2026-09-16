dedddbe90888b92893affe8ad1ec06785ceadf3f
Gwen Williams <gwyneth.s.williams@gmail.com>
2026-09-16 21:27:41 +0100

    Capitalize set id

--- ui/home/src/main/kotlin/com/arcryalis/gwentest/home/HomeScreen.kt
+++ ui/home/src/main/kotlin/com/arcryalis/gwentest/home/HomeScreen.kt
@@ -15,9 +15,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -129,7 +127,7 @@ private fun HomeReadyScreen(
                     .padding(vertical = 8.dp)
             ) {
                 Text(
-                    text = "$name (${id.capitalize(Locale.current)})",
+                    text = "$name (${id.uppercase()})",
                     fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Center,
